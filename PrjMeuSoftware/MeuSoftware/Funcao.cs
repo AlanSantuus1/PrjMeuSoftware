11af5953c094f46e3aaedb700d542298792c3c62
Fernando <fernando.silva789@etec.sp.gov.br>
2019-05-30 13:01:53 -0300

    Adicionado uma função para o cálculo

---
+++ PrjMeuSoftware/MeuSoftware/Funcao.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeuSoftware
+{
+    class Funcao
+    {
+        public int CalcularIdade(DateTime dataNascimento, DateTime hoje)
+        {
+            int idade = hoje.Year - dataNascimento.Year;
+
+            if (hoje.Month < dataNascimento.Month || (hoje.Month == dataNascimento.Month && hoje.Day < dataNascimento.Day))
+
+                idade--;
+
+            return idade;
+        }
+
+    }
+}
